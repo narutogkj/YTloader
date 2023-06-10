1d69a325a45f3ce4872f19e9d48701afa1f3564b
Gaurav Jha <gkj498@gmail.com>
2023-06-10 10:00:48 +0530

    used query param

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const ytdl = require('ytdl-core')
 const app = express()
 const port = 3000
 
-app.get('/download', (req, res) => {
-  const url = req.query.url
-  const name = req.query.url
-  res.header('Content-Disposition', `attachment; filename="${name}.mp4"`)
+app.get('/', (req, res) => {
+  const url = req.query.d
+  res.header('Content-Disposition', `attachment; filename="my${Date.now()}.mp4"`)
   ytdl(url, { format: 'mp4' }).pipe(res)
 })
 
