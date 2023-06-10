897b19dcfa310d145406ff11afdbcd5bf44d0441
Gaurav Jha <gkj498@gmail.com>
2023-06-10 13:09:05 +0530

    handle request without youtube link

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  const url = req.query.d
-  res.header('Content-Disposition', `attachment; filename="my${Date.now()}.mp4"`)
-  ytdl(url, { format: 'mp4' }).pipe(res)
+  if( req.query.d ){
+    const url = req.query.d
+    res.header('Content-Disposition', `attachment; filename="my${Date.now()}.mp4"`)
+    ytdl(url, { format: 'mp4' }).pipe(res)
+  }else{
+    res.send("d required ")
+  }
+ 
 })
 
 app.listen(port, () => {
